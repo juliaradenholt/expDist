7eea4ec177eedb3c12811476df13c36c36f5eaa4
juliaradenholt <48519929+juliaradenholt@users.noreply.github.com>
2021-02-10 14:36:25 +0100

    Create run_expdist.py

---
+++ run_expdist.py
@@ -0,0 +1,26 @@
+import expected_distance as e
+import models as m
+import random
+
+models = { 'Dayhoff':m.Dayhoff(), 'JTT':m.JTT(), 'LG':m.LG(), 'WAG':m.WAG() }
+case= ['W', 'X', 'Y', 'Z']
+models_str = ['Dayhoff', 'JTT', 'LG', 'WAG']
+test_case = (random.randint(0, 3))
+test_num = (random.randint(1, 35))
+test_mod = (random.randint(0, 3))
+
+print("Enter path to file, if nothing is entered,")
+print("a random file from the testcases is picked.")
+filename = input()
+print("Which substiution model do you want to use?")
+print("Please enter: Dayhoff, JTT, LG or WAG")
+model_name = input()
+
+try:
+    model = (models.get(model_name))
+    filepath = filename if filename != '' else ("./testcases/"+case[test_case]+"/"+str(test_num)+'/m'+models_str[test_case])+".fasta"
+    filepath = filename if filename != '' else ("./testcases/"+case[test_case]+"/"+str(test_num)+'/m'+models_str[test_case])+".fasta"
+    print("Running "+str(filepath)+" with "+str(model_name))
+    expected_value = e.expected_dist(filepath, model)
+except AttributeError or TypeError:
+    print("Please enter a valid model.")
