f4a28a25bc2800539002edb1232c28ae3a6bddad
juliaradenholt <48519929+juliaradenholt@users.noreply.github.com>
2021-02-10 15:03:02 +0100

    Update run_expdist.py
    program which allows user input

--- run_expdist.py
+++ run_expdist.py
@@ -9,11 +9,13 @@ test_case = (random.randint(0, 3))
 test_num = (random.randint(1, 35))
 test_mod = (random.randint(0, 3))
 
-print("Enter path to file, if nothing is entered,")
-print("a random file from the testcases is picked.")
+print("\n \nThis program allows you to enter a substiution model and a filepath.")
+print("The program will then estimate and print the expected values of the evolutionary distances")
+print("between the sequences in the file. The only valid format for the files is the FASTA format.\n \n")
+
+print("Enter path to file, if nothing is entered, a random file from the testcases is picked:")
 filename = input()
-print("Which substiution model do you want to use?")
-print("Please enter: Dayhoff, JTT, LG or WAG")
+print("Which substiution model do you want to use? Please enter: Dayhoff, JTT, LG or WAG:")
 model_name = input()
 
 try:
@@ -23,4 +25,4 @@ try:
     print("Running "+str(filepath)+" with "+str(model_name))
     expected_value = e.expected_dist(filepath, model)
 except AttributeError or TypeError:
-    print("Please enter a valid model.")
+    print("Please enter a valid model and filepath.")
